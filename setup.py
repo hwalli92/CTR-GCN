359d83910fada4f15c6699c59b804f5e4e467507
Hasnainali Walli <hwalli92@gmail.com>
2022-09-13 12:42:28 +0200

    create CTR-GCN as module

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='CTRGCN',
+      version='1.0.0',
+      description='CTR-GCN Action Recognition Network for the LAL-4-SARs',
+      package_dir={'': '.'}
+      )
